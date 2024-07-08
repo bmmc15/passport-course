14ddf34e0f5e469bb3948da562f2940859a34244
BernardoCamarate <bernardocamarate@RUNTIMEs-Air.lan>
2024-07-08 17:55:06 +0100

    Added pre hook and simplified some code

--- passport-jwt-strategy/models/user.js
+++ passport-jwt-strategy/models/user.js
@@ -1,9 +1,37 @@
 const mongoose = require("mongoose");
+const utils = require("../lib/utils");
 
 const UserSchema = new mongoose.Schema({
   username: String,
   hash: String,
   salt: String,
 });
+
+UserSchema.virtual("password")
+  .set(function (password) {
+    this._password = password;
+  })
+  .get(function () {
+    this._password;
+  });
+
+UserSchema.pre("save", function (next) {
+  if (this._password) {
+    const { salt, hash } = utils.genPassword(this._password);
+
+    this._password = undefined;
+    this.hash = hash;
+    this.salt = salt;
+  }
+  next();
+});
+
+UserSchema.methods.isValidPassword = function (password) {
+  return utils.validPassword(password, this.hash, this.salt);
+};
+
+UserSchema.methods.issueJwt = function () {
+  return utils.issueJWT(this);
+};
 
 mongoose.model("User", UserSchema);
--- passport-jwt-strategy/routes/users.js
+++ passport-jwt-strategy/routes/users.js
@@ -1,61 +1,52 @@
 const mongoose = require("mongoose");
 const router = require("express").Router();
 const User = mongoose.model("User");
-const passport = require("passport");
-const utils = require("../lib/utils");
 
 router.get("/protected", (req, res, next) => {});
 
-router.post("/login", (req, res, next) => {
+router.post("/login", async function (req, res, next) {
   const { username, password } = req.body;
-  User.findOne({ username })
-    .then((user) => {
-      if (!user) {
-        res.status(401).json({
-          success: false,
-          msg: "Could not find user",
-        });
-      }
-      const isValid = utils.validPassword(password, user.hash, user.salt);
-      if (isValid) {
-        const tokenObject = utils.issueJWT(user);
-        res.status(200).json({
-          success: true,
-          user,
-          token: tokenObject.token,
-          expiresIn: tokenObject.expires,
-        });
-      } else {
-        res
-          .status(401)
-          .json({ success: true, msg: "You entered the wrong password" });
-      }
-    })
-    .catch((err) => next(err));
-});
-router.post("/register", function (req, res, next) {
-  const { username, password } = req.body;
+  const user = await User.findOne({ username });
 
-  const { salt, hash } = utils.genPassword(password);
+  if (!user) {
+    res.status(401).json({
+      success: false,
+      msg: "Could not find user",
+    });
+  }
+  const isValid = user.isValidPassword(password);
+  if (isValid) {
+    const tokenObject = user.issueJwt();
+    res.status(200).json({
+      success: true,
+      user,
+      token: tokenObject.token,
+      expiresIn: tokenObject.expires,
+    });
+  } else {
+    res
+      .status(401)
+      .json({ success: true, msg: "You entered the wrong password" });
+  }
+});
+
+router.post("/register", async function (req, res, next) {
+  const { username, password } = req.body;
 
   const newUser = new User({
     username,
-    salt,
-    hash,
+    password: password,
   });
 
-  newUser
-    .save()
-    .then((user) => {
-      const jwt = utils.issueJWT(user);
-      res.json({
-        success: true,
-        user,
-        token: jwt.token,
-        expiresIn: jwt.expires,
-      });
-    })
-    .catch((err) => next(err));
+  const savedUser = await newUser.save();
+
+  const jwt = savedUser.issueJwt();
+  res.json({
+    success: true,
+    user: savedUser,
+    token: jwt.token,
+    expiresIn: jwt.expires,
+  });
 });
 
 module.exports = router;
